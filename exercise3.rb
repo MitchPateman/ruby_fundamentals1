5f3a1f94e9bad09e6fcb464cfaacd9d765aba431
Mitch Pateman <mitch_pateman@hotmail.com>
2016-05-31 14:32:37 -0400

    Modified exercise 3 to fix age part

--- exercise3.rb
+++ exercise3.rb
@@ -2,4 +2,4 @@ puts "What is your name?"
 name = gets.chomp
 puts "How old are you?"
 age = gets.chomp.to_i
-puts "Hello #{name}, you are #{age} years old"
+puts "Hello #{name}, you were born in #{2016 - age}"
