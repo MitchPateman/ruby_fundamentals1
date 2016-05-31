23e74669a09eb7f078e2a6e28773ca2c31f103f1
Mitch Pateman <mitch_pateman@hotmail.com>
2016-05-31 14:18:39 -0400

    Adding exercise 2 and an empty ex3

---
+++ exercise2.rb
@@ -0,0 +1,10 @@
+tip = 55 * 0.15
+puts "a good tip for a $55 meal is #{tip}"
+
+string = "Mitch"
+integer = 25
+puts string + integer.to_s
+
+puts "45628 x 7839 = #{45628 * 7839}"
+
+puts (10 < 20 && 30 < 20) || !(10 == 11)
