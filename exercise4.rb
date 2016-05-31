f028d80d51a960c089ccf479ebb0ddca5774e157
Mitch Pateman <mitch_pateman@hotmail.com>
2016-05-31 14:48:19 -0400

    Adding finished exercise4

---
+++ exercise4.rb
@@ -0,0 +1,19 @@
+counter = 0
+
+while counter <= 100
+  if counter % 3 ==0 && counter % 5 == 0
+    puts "BitMaker"
+
+  elsif counter % 3 == 0 && counter % 5 != 0
+    puts "Bit"
+
+  elsif counter % 5 == 0 && counter % 3 != 0
+    puts "Maker"
+
+  else
+    puts "#{counter}"
+  end
+
+counter += 1
+
+end
